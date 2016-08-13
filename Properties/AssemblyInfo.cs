74d2edee2723507e8ff998f12be942fd11f6fc5d
CrazedProgrammer <crazedprogrammer@gmail.com>
2016-08-14 00:35:24 +0200

    .

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSKrist")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("KristSharp")]
+[assembly: AssemblyDescription("An object-oriented Krist API wrapper for C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CrazedProgrammer")]
 [assembly: AssemblyProduct("KristSharp")]
