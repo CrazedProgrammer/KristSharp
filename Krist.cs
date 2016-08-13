6bb6d0860e9fdfc8f4eceb3dcef0ac9efc999be4
CrazedProgrammer <crazedprogrammer@gmail.com>
2016-08-14 00:41:01 +0200

    .

--- Krist.cs
+++ Krist.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Collections;
-using System.Data;
 using System.Globalization;
 
 namespace Crazed.KristSharp
